4791c455a0880c8bbd160eb8f5c80bc63e2defeb
Nolan L <nolan.s.locke@gmail.com>
2025-12-01 23:06:04 -0700

    add template

---
+++ src/main/kotlin/Template.kt
@@ -0,0 +1,22 @@
+import util.Util
+
+private fun main() {
+    val demoInput = """
+    
+    """.trimIndent().lines()
+
+    val input =
+        Util.loadInputLines(2025, TODO("Insert Date!"))
+//        demoInput
+
+    var p1 = 0
+    var p2 = 0
+
+
+    for (ln in input) {
+
+    }
+
+
+    Util.printSolution(TODO("Insert Date!"), p1, p2)
+}
